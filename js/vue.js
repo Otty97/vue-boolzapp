bd679e4aa5eef1d0af196070e92e95f17664984c
Othman Taik <taikothman1997@gmail.com>
2020-11-20 14:31:16 +0100

    HTML Css structure + Js concept

--- js/vue.js
+++ js/vue.js
@@ -1,4 +1,18 @@
-const myApp = new Vue({
-    el: "#root",
-    data: {
-        
+//Input App Messagistica
+MY_PROFIL -> foto, nome,
+CONTATTI -> CONTATTO-1, CONTATTO-2, ..., CONTATTO-N
+CONTATTO-N -> stesse proprieta di MY_PROFIL[] + ULTIMO ACCESSO
+
+//Input -> Arrey di proprietà
+const utentePrincipale = {
+	foto: tipo stringa / url ,
+	nome: tipo stringa / generica
+}
+
+var contatti = [contatto * contatto.length]
+
+const contatto  = {
+  foto: tipo stringa / url ,
+	nome: tipo stringa / generica,
+	ultimoAccesso: tipo stringa / rappresentazione di una data
+}
